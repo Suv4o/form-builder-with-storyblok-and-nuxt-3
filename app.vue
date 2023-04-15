2a75510aa6582470f6b403fd8cdba3bd4f2d92ce
Aleks <aleksandar.trpkovski@gmail.com>
2023-04-15 11:28:52 +1000

    Setup StoryBlok

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+    <div>
+        <h1 class="text-3xl font-bold underline">My Dynamic Form!</h1>
+    </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    css: ["~/assets/css/main.css"],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
+    modules: ["@storyblok/nuxt"],
+    storyblok: {
+        accessToken: process.env.STORYBLOK_API_TOKEN,
+    },
     typescript: {
         typeCheck: true,
         strict: true,
     },
-})
+});
