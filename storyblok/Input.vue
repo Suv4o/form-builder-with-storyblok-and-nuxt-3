db8df1fe501b4ecbdac6acbae7b259631f28e462
Aleks <aleksandar.trpkovski@gmail.com>
2023-04-18 22:05:31 +1000

    create Input without logic

--- storyblok/Input.vue
+++ storyblok/Input.vue
@@ -1,15 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const props = defineProps<{
+    blok: {
+        label: string;
+        name: string;
+        placeholder: string;
+        type: string;
+        required: boolean;
+        _uid: string;
+    };
+}>();
+</script>
 
 <template>
     <div>
-        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email</label>
+        <label :for="blok._uid" class="block text-sm font-medium leading-6 text-gray-900">{{ blok.label }}</label>
         <div class="mt-2">
             <input
-                type="email"
-                name="email"
-                id="email"
+                :type="blok.type"
+                :name="blok.name"
+                :id="blok._uid"
                 class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
-                placeholder="you@example.com"
+                :placeholder="blok.placeholder"
             />
         </div>
     </div>
