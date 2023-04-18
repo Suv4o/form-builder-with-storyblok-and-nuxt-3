8addda5c6b6810995a8529677fa5fb0bb0be5a45
Aleks <aleksandar.trpkovski@gmail.com>
2023-04-18 22:08:15 +1000

    added padding left and right

--- app.vue
+++ app.vue
@@ -4,7 +4,7 @@ const story = await fetchStoriesBySlug(`/home`);
 </script>
 
 <template>
-    <div class="container mx-auto">
+    <div class="container mx-auto px-4">
         <StoryblokComponent v-if="story" :blok="story.content" />
     </div>
 </template>
